2b3bc4881a57f142d2a9e6c95054131a79bfc405
jbonez87 <jcastrillon087@gmail.com>
2019-02-19 23:06:09 -0500

    add concertsReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,8 +11,23 @@ export const fetchConcerts = (query) => async dispatch => {
     type: FETCHING_CONCERTS
   })
   try {
-    
+    const request = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${key}&zip=${query}`);
+    const response = await request.json();
+    if (!request.ok) {
+      dispatch({
+        type: FETCHING_CONCERTS_REJECTED,
+        payload: request.statusText
+      });
+    } else {
+      dispatch({
+        type: FETCHING_CONCERTS_RESOLVED,
+        payload: response
+      });
+    }
   } catch (error) {
-    
+    dispatch({
+      type: FETCHING_CONCERTS_REJECTED,
+      payload: e
+    });
   }
 }
