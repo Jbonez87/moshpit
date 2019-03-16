411f7def8cf7b91aacf89fe7dc1282713bb48d07
jbonez87 <jcastrillon087@gmail.com>
2019-03-16 07:29:57 -0400

    add skeleton styling to about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import '../static/css/about.css';
 
 const About = () => (
-  <div>
+  <div
+    className="about-container"
+  >
     <p>
-      Whether it's a concert, a ball game or a play, it should never cost <br />
+      Whether it's a concert, a ball game or a play, it should never cost
       thousands of dollars to have a good time.<br />
+      <br />
       Here at MoshPit, we're all about bringing people together and keeping<br />
       costs low, for both the venue and the customer.
     </p>
