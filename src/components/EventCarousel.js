3b835551dd3019e8820f3788adfdc1bbbd8e5150
jbonez87 <jcastrillon087@gmail.com>
2019-03-17 14:37:51 -0400

    add EventCarousel component

---
+++ src/components/EventCarousel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventCarousel extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default EventCarousel;
