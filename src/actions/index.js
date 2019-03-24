9901063482a64f18a45822d5d238fd6108cf5c23
Jbonez87 <jcastrillon087@gmail.com>
2019-03-24 12:09:59 -0400

    refactor concert actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -24,68 +24,78 @@ export const fetchConcertsByZip = query => async dispatch => {
   dispatch({
     type: FETCHING_CONCERTS
   })
-  try {
-    let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${key}&postalCode=${query}`;
-    const request = await fetch(url);
-    const response = await request.json();
-    if (!request.ok) {
-      dispatch({
-        type: FETCHING_CONCERTS_REJECTED,
-        payload: request.statusText
-      });
-    } else if(!query) {
-      dispatch({
-        type: FETCHING_CONCERTS_REJECTED,
-        payload: 'Please do not leave form blank'
-      })
-    } else if (!response._embedded) {
-      dispatch({
-        type: FETCHING_CONCERTS_REJECTED,
-        payload: 'No events found'
-      })
-    } else {
-      dispatch({
-        type: FETCHING_CONCERTS_RESOLVED,
-        payload: formatEventsResponse(response)
-      });
-    }
-  } catch (e) {
+  if (!query) {
     dispatch({
       type: FETCHING_CONCERTS_REJECTED,
-      payload: e
-    });
+      payload: 'Please do not leave form blank'
+    })
+  } else {
+    try {
+      let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${key}&postalCode=${query}`;
+      const request = await fetch(url);
+      const response = await request.json();
+      if (!request.ok) {
+        dispatch({
+          type: FETCHING_CONCERTS_REJECTED,
+          payload: request.statusText
+        });
+      } else if (!response._embedded) {
+        dispatch({
+          type: FETCHING_CONCERTS_REJECTED,
+          payload: 'No events found'
+        })
+      } else {
+        dispatch({
+          type: FETCHING_CONCERTS_RESOLVED,
+          payload: formatEventsResponse(response)
+        });
+      }
+    } catch (e) {
+      dispatch({
+        type: FETCHING_CONCERTS_REJECTED,
+        payload: e
+      });
+    }
   }
+  
 }
 
 export const fetchConcertsByCity = query => async dispatch => {
   dispatch({
     type: FETCHING_CONCERTS
   })
-  try {
-    let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${key}&city=${query}`;
-    const request = await fetch(url);
-    const response = await request.json();
-    if (!request.ok) {
-      dispatch({
-        type: FETCHING_CONCERTS_REJECTED,
-        payload: request.statusText
-      });
-    } else if(!response._embedded) {
-      dispatch({
-        type: FETCHING_CONCERTS_REJECTED,
-        payload: 'No events found'
-      })
-    } else {
-      dispatch({
-        type: FETCHING_CONCERTS_RESOLVED,
-        payload: formatEventsResponse(response)
-      });
-    }
-  } catch (e) {
+  if (!query) {
     dispatch({
       type: FETCHING_CONCERTS_REJECTED,
-      payload: e
-    });
+      payload: 'Please do not leave form blank'
+    })
+  } else {
+    try {
+      let url = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${key}&city=${query}`;
+      const request = await fetch(url);
+      const response = await request.json();
+      if (!request.ok) {
+        dispatch({
+          type: FETCHING_CONCERTS_REJECTED,
+          payload: request.statusText
+        });
+      } else if (!response._embedded) {
+        dispatch({
+          type: FETCHING_CONCERTS_REJECTED,
+          payload: 'No events found'
+        })
+      } else {
+        dispatch({
+          type: FETCHING_CONCERTS_RESOLVED,
+          payload: formatEventsResponse(response)
+        });
+      }
+    } catch (e) {
+      dispatch({
+        type: FETCHING_CONCERTS_REJECTED,
+        payload: e
+      });
+    }
   }
 }
 
