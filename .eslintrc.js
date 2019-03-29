6e7d2d54d75465934331cb0bebbb34abcff86c4c
Jbonez87 <jcastrillon087@gmail.com>
2019-03-29 07:10:04 -0400

    disable jsx eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,10 @@ module.exports = {
     ],
     "env": {
         "browser": true
+    },
+    "rules": {
+        "react/jsx-filename-extension": [1, {
+            "extensions": [".js", ".jsx"]
+        }],
     }
 };
