fc24abbdf73afcd54c21ca4315b2c39bca653538
Jbonez87 <jcastrillon087@gmail.com>
2019-03-30 09:40:27 -0400

    add new minimum width and height for event images

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,11 +3,11 @@ import React from 'react';
 import loadingGif from '../static/images/heavylogointrohorizonatal_done.gif';
 
 const Loading = () => (
-    <img
-      className="loading" 
-      src={loadingGif}
-      alt="Loading..."
-    />
+  <img
+    className="loading"
+    src={loadingGif}
+    alt="Loading..."
+  />
 );
 
 export default Loading;
