4e51981273a2b16049387abe6fac214f4d1e89eb
Jbonez87 <jcastrillon087@gmail.com>
2019-04-07 22:30:50 -0400

    add graphql schema types

--- api/schema.js
+++ api/schema.js
@@ -1,10 +1,20 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString } = graphql;
 
 const EventType = new GraphQLObjectType({
   name: 'Events',
   fields: () => ({
-    
-  })
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    saleDate: { type: GraphQLString },
+    info: { type: GraphQLString },
+  }),
 });
+
+const RootQuery = new GraphQLObjectType({
+
+});
+
+module.exports = EventType;
