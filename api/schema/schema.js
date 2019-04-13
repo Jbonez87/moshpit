1f6cc6b31cc54bd293b861985db6cd12c4b9c34f
Jbonez87 <jcastrillon087@gmail.com>
2019-04-12 22:18:45 -0400

    remove events dummy data

--- api/schema/schema.js
+++ api/schema/schema.js
@@ -12,58 +12,6 @@ const {
   GraphQLList,
 } = graphql;
 
-// dummy data
-// const events = [
-//   {
-//     id: '1',
-//     name: 'Epicenter Festival',
-//     genre: 'Rock',
-//     saleDate: '2019-04-12T14:00:00Z',
-//     info: 'TBA',
-//     venueId: '1',
-//   },
-//   {
-//     id: '2',
-//     name: 'Colorado Rockies vs. Los Angeles Dodgers',
-//     genre: 'Sports',
-//     saleDate: '2019-01-23T17:00:00Z',
-//     info: 'TBA',
-//     venueId: '2',
-//   },
-//   {
-//     id: '3',
-//     name: 'Disney On Ice presents Dare To Dream',
-//     genre: 'Play',
-//     saleDate: '2018-10-30T14:00:00Z',
-//     info: 'TBA',
-//     venueId: '3',
-//   },
-//   {
-//     id: '4',
-//     name: 'Old Dominion',
-//     genre: 'Rock',
-//     saleDate: '2019-01-18T15:00:00Z',
-//     info: 'TBA',
-//     venueId: '3',
-//   },
-//   {
-//     id: '5',
-//     name: 'Billy Joel',
-//     genre: 'Rock',
-//     saleDate: '2019-01-18T17:00:00Z',
-//     info: 'TBA',
-//     venueId: '2',
-//   },
-//   {
-//     id: '6',
-//     name: 'Zac Brown Band: The Owl Tour',
-//     genre: 'Rock',
-//     saleDate: '2019-02-08T17:00:00Z',
-//     info: 'TBA',
-//     venueId: '2',
-//   },
-// ];
-
 const EventType = new GraphQLObjectType({
   name: 'Events',
   fields: () => ({
