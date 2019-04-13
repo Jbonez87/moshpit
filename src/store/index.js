5bb1e09d23feb413bb11fa1b7e2efccc96b27fed
Jbonez87 <jcastrillon087@gmail.com>
2019-04-13 11:16:05 -0400

    fix eslint errors

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,39 @@
 import {
   createStore,
   applyMiddleware,
-  compose
+  compose,
 } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import {
   loadState,
-  saveState
+  saveState,
 } from './localStorage';
 import rootReducer from '../reducers';
 
 let middleware;
-if(process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
   middleware = [
     thunk,
-    logger
-  ]
+    logger,
+  ];
 } else {
   middleware = [
-    thunk
-  ]
+    thunk,
+  ];
 }
 
 const persistedState = loadState();
+// eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   persistedState,
   composeEnhancers(
-    applyMiddleware(...middleware)
-  )
+    applyMiddleware(...middleware),
+  ),
 );
 
 store.subscribe(() => saveState(store.getState()));
--- src/store/localStorage.js
+++ src/store/localStorage.js
@@ -8,13 +8,14 @@ export const loadState = () => {
   } catch (e) {
     return undefined;
   }
-}
+};
 
-export const saveState = state => {
+// eslint-disable-next-line consistent-return
+export const saveState = (state) => {
   try {
     const serializedState = JSON.stringify(state);
     localStorage.setItem('state', serializedState);
   } catch (e) {
     return undefined;
   }
-}
+};
