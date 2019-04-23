514fc66115150540f988abc954956e0ae417524b
Jbonez87 <jcastrillon087@gmail.com>
2019-04-22 21:06:19 -0400

    add EventList component

---
+++ src/components/EventList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class EventList extends Component {
+  render() {
+    return (
+      <div
+        className="event-list"
+      >
+        
+      </div>
+    );
+  }
+}
+
+export default EventList;
